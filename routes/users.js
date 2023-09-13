7e5101159ca9a7607a22c4db4f25bce074e55020
Svitlana-Lazurenko <115888343+Svitlana-Lazurenko@users.noreply.github.com>
2023-09-13 17:16:41 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,8 @@ router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
 
 router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
 
+router.get('/current', authenticate, ctrl.getCurrent);
+
 router.post('/logout', authenticate, ctrl.logout);
 
 module.exports = router;
